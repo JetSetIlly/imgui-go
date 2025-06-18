66635c61cf969add897a5e24d1ddf2d7e78471be
JetSetIlly <stephen.t.illingworth@gmail.com>
2025-06-18 18:30:43 +0100

    migration of DrawData to 1.91.9b

--- wrapper/ConfiguredImGui.h
+++ wrapper/ConfiguredImGui.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <stdint.h>
+
 // As recommended in imgui/imconfig.h, the IMGUI_USER_CONFIG must be specified
 // whenever imgui is used. As such, the wrapper code should never include imgui.h directly.
-
 #define IMGUI_USER_CONFIG "wrapper/ConfigOverride.h"
+
+#define IMGUI_DEFINE_MATH_OPERATORS 1
+#define ImTextureID uintptr_t
+
+// IM_OFFSET_OF was obsoleted in dearimgui 1.90. we redefine it here
+#define IM_OFFSETOF(_TYPE,_MEMBER)  offsetof(_TYPE, _MEMBER)
+
 #include "imgui.h"
